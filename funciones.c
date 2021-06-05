29735902c7ba0afc9479128d64a971f61ad08024
ribrl <>
2021-06-05 21:06:24 +0000

    distancia entre entregas (funcion 2)

--- funciones.c
+++ funciones.c
@@ -1,50 +1,100 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include "list.h"
 #include "funciones.h"
 
 Nodo* importarArchivo(char* file_name, int n){
-  Nodo* r = (Nodo*) malloc(sizeof(Nodo));;
-  FILE* file = fopen (file_name, "r");
-  int i,id=1;
-  for(i=0;i<n;i++){
-   r->id[i]=id;
-   fscanf (file, "%d", &r->x[i]);
-   fscanf (file, "%d", &r->y[i]);
-   printf("%d ",r->id[i]);
-   printf("%d",r->x[i]);
-   printf(" %d\n",r->y[i]);
-   id++;
-  }
+	Nodo* r = (Nodo*) malloc(sizeof(Nodo));;
+	FILE* file = fopen (file_name, "r");
+	int i,id=1;
+	for(i=1;i<=n;i++){
+		r->id[i]=id;
+		fscanf (file, "%d", &r->x[i]);
+		fscanf (file, "%d", &r->y[i]);
+		printf("%d ",r->id[i]);
+		printf("%d",r->x[i]);
+		printf(" %d\n",r->y[i]);
+		id++;
+	}
 
-  fclose (file);
-  return r;
+	fclose (file);
+	return r;
 }
 
-/*List* get_adj_nodes(Nodo* r, int n){
+List* get_adj_nodes(Nodo* r, int n){
     
     List* list=createList();
-    int i,j,k;
+    int i;
     for(i=0;i<n;i++){
       
-      for(k=1;k<10;k++){
-      Nodo *adj_r=(Nodo*)malloc(sizeof(Nodo));
-      adj_r=r;
-      pushBack(list,adj_r);
-      }
+      
+    Nodo *adj_r=(Nodo*)malloc(sizeof(Nodo));
+    adj_r=r;
+    pushBack(list,adj_r);
+      
 
-     return list;
+    	return list;
     }
     
     return list;
-}*/
+}
 
-/*void crearRuta(){
+void distanciaEntreEntregas(Nodo* r,int n){
+int id,id2;
+float distancia;
+
+printf("Inserte el identificador de la primera entrega\n");
+scanf("%d", &id);
+
+printf("Inserte el identificador de la segunda entrega\n");
+scanf("%d", &id2);
+
+if(id>n || id2>n || id<1 || id2<1){
+printf("A ingresado un id incorrecto\n");
+
+}else{
+distancia=sqrt((r->x[id2] - r->x[id])*(r->x[id2] - r->x[id])+(r->y[id2]-r->y[id])*(r->y[id2] - r->y[id]));
+
+printf("La distancia entre las 2 entregas ingresadas es\n");
+printf("%.2f", distancia);
 
 }
 
-void generarRutaAleatoria(){
+
+}
+
+/*void mostrar3Entregas(){
+  
+}*/
+
+/*void crearRuta(Nodo* r,int n){
+int x,y,i,tru=0;
+char* nombreRuta;
+printf("Ingrese la coordenada para la variable x\n");
+scanf("%d", &x);
+printf("Ingrese la coordenada para la variable y\n");
+scanf("%d", &y);
+for(i=0; i<n; i++){
+	if(x==r->x[i] && y==r->y[i]){
+		tru=1;
+		}
+	}
+
+	if(tru==1){
+		    
+    printf("Ïngrese el nombre a su nueva ruta\n");
+		scanf("%s", nombreRuta);
+	}else{
+		printf("Las coordenadas ingreseada no existen");
+	}
+	
+}*/
+
+
+/*
+void generarRutaAleatoria(Nodo* r){
 
 }
 */
